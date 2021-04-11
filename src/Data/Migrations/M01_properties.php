cade4e8bbd1d7cebd25654e85ce4852d1b25bf8a
Vladislav Osmianskij <vlaosm@gmail.com>
2021-04-12 00:41:41 +0300

    minor: core data schema is extracted into `properties.json`

--- src/Data/Migrations/M01_properties.php
+++ src/Data/Migrations/M01_properties.php
@@ -9,9 +9,11 @@ use Illuminate\Database\Schema\Blueprint;
 use Osm\Core\App;
 use Osm\Framework\Db\Db;
 use Osm\Framework\Migrations\Migration;
+use function Osm\object_empty;
 
 /**
  * @property Db $db
+ * @property \stdClass $root_property
  */
 class M01_properties extends Migration
 {
@@ -38,7 +40,7 @@ class M01_properties extends Migration
         $this->db->drop('properties');
     }
 
-    public function insert(): void {
+    public function insert_old(): void {
         $properties = $this->db->table('properties')->insertGetId([
             'endpoint' => '/properties',
             'data' => json_encode((object)[
@@ -223,5 +225,91 @@ class M01_properties extends Migration
             ]),
         ]);
 
+    }
+
+    protected function get_root_property(): \stdClass {
+        return json_decode(file_get_contents(__DIR__ . '/properties.json'));
+    }
+
+    public function insert(): void {
+        foreach ($this->root_property as $name => $property) {
+            $this->insertProperty($name, $property, null);
+        }
+    }
+
+    protected function insertProperty(string $name, \stdClass $property,
+        ?int $parentId): void
+    {
+        switch ($property->type) {
+            case 'object':
+                $this->insertObject($name, $property, $parentId);
+                break;
+            case 'array':
+                $this->insertArray($name, $property, $parentId);
+                break;
+            default:
+                $this->insertScalar($name, $property, $parentId);
+                break;
+        }
+    }
+
+    protected function insertObject(string $name, \stdClass $property,
+        ?int $parentId): void
+    {
+        $id = $this->db->table('properties')->insertGetId(
+            $this->values($name, $property, $parentId));
+
+        foreach ($property->properties ?? [] as $key => $value) {
+            $this->insertProperty($key, $value, $id);
+        }
+    }
+
+    protected function insertArray(string $name, \stdClass $property,
+        ?int $parentId): void
+    {
+        $id = $this->db->table('properties')->insertGetId(
+            $this->values($name, $property, $parentId));
+
+        foreach ($property->items->properties ?? [] as $key => $value) {
+            $this->insertProperty($key, $value, $id);
+        }
+    }
+
+    protected function insertScalar(string $name, \stdClass $property,
+        ?int $parentId): void
+    {
+        $this->db->table('properties')->insert(
+            $this->values($name, $property, $parentId));
+    }
+
+    protected function values(string $name, \stdClass $property,
+        ?int $parentId): array
+    {
+        $values = ['parent_id' => $parentId];
+        $data = (object)['name' => $name];
+
+        foreach ($property as $key => $value) {
+            if ($key == 'endpoint') {
+                $values[$key] = $value;
+                continue;
+            }
+
+            if ($key == 'properties') {
+                continue;
+            }
+
+            if ($key == 'items') {
+                $value = clone $value;
+                unset($value->properties);
+            }
+
+            $data->$key = $value;
+        }
+
+        if (!object_empty($data)) {
+            $values['data'] = json_encode($data);
+        }
+
+        return $values;
     }
 }
